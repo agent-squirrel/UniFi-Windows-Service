c5bbd79c81e02fd7ace70921a066e1a585d813c0
Adam Heathcote <aheathcote@linux.com>
2018-04-12 17:26:16 +1000

    Update install.bat

--- install.bat
+++ install.bat
@@ -62,7 +62,7 @@ echo.
 
 echo Downloading dependencies
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://esperto.com.au/repo/jre/windows/x64/latest.exe', '%temp%\jre.exe')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('http://dl.ubnt.com/unifi/5.5.20/UniFi-installer.exe', '%temp%\unifi.exe')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://dl.ubnt.com/unifi/5.7.20/UniFi-installer.exe', '%temp%\unifi.exe')"
 echo.
 echo Installing dependencies
 start /w %temp%\jre.exe /s
